05f761ccc5a1a543202e6a51bef5992d8fa09804
TracksJosh <arcticwolf1526@gmail.com>
2021-09-14 21:34:55 -0500

    audio goes brrrr
    Fixed audio bug and added autosolver

--- Assets/Rejects/myMom/MyMomScript.cs
+++ Assets/Rejects/myMom/MyMomScript.cs
@@ -18,6 +18,8 @@ public class MyMomScript : MonoBehaviour
     public Material WhiteButton;
     public Material RedButton;
 
+    int shift = 0;
+
     // This is the arrangement of the letters in My Mom.
     static char[,] symbolTable = new char[,] {
         {'M', 'Y', ' '},
@@ -25,6 +27,11 @@ public class MyMomScript : MonoBehaviour
     };
 
     private char[,] newSymbolTable = new char[,] {
+        {'M', 'Y', ' '},
+        {'M', 'O', 'M'}
+    };
+
+    private char[,] solveSymbolTable = new char[,] {
         {'M', 'Y', ' '},
         {'M', 'O', 'M'}
     };
@@ -48,16 +55,16 @@ public class MyMomScript : MonoBehaviour
         moduleId = moduleIdCounter++;
         GetComponent<KMBombModule>().OnActivate += OnActivate;
 
-        buttons[0].OnInteract += delegate { RotateVertical(buttons[0], 1); return false; };
-        buttons[1].OnInteract += delegate { RotateVertical(buttons[1], 1); return false; };
-        buttons[2].OnInteract += delegate { RotateVertical(buttons[2], 2); return false; };
-        buttons[3].OnInteract += delegate { RotateVertical(buttons[3], 2); return false; };
-        buttons[4].OnInteract += delegate { RotateVertical(buttons[4], 3); return false; };
-        buttons[5].OnInteract += delegate { RotateVertical(buttons[5], 3); return false; };
-        buttons[6].OnInteract += delegate { RotateHorizontal(buttons[6], 0, -1); return false; };
-        buttons[7].OnInteract += delegate { RotateHorizontal(buttons[7], 0, 1); return false; };
-        buttons[8].OnInteract += delegate { RotateHorizontal(buttons[8], 1, -1); return false; };
-        buttons[9].OnInteract += delegate { RotateHorizontal(buttons[9], 1, 1); return false; };
+        buttons[0].OnInteract += delegate { return RotateVertical(display, buttons[0], 1); };
+        buttons[1].OnInteract += delegate { return RotateVertical(display, buttons[1], 1); };
+        buttons[2].OnInteract += delegate { return RotateVertical(display, buttons[2], 2); };
+        buttons[3].OnInteract += delegate { return RotateVertical(display, buttons[3], 2); };
+        buttons[4].OnInteract += delegate { return RotateVertical(display, buttons[4], 3); };
+        buttons[5].OnInteract += delegate { return RotateVertical(display, buttons[5], 3); };
+        buttons[6].OnInteract += delegate { return RotateHorizontal(display, buttons[6], 0, -1); };
+        buttons[7].OnInteract += delegate { return RotateHorizontal(display, buttons[7], 0, 1); };
+        buttons[8].OnInteract += delegate { return RotateHorizontal(display, buttons[8], 1, -1); };
+        buttons[9].OnInteract += delegate { return RotateHorizontal(display, buttons[9], 1, 1); };
 
         /*
         for (int i = 0; i < 10; i += 2) {
@@ -91,12 +98,21 @@ public class MyMomScript : MonoBehaviour
         display[1, 1] = characters[(display[1, 1] + cipher) % 26];
         display[1, 2] = characters[(display[1, 2] + cipher) % 26];
 
+        solveSymbolTable[0, 0] = display[0, 0];
+        solveSymbolTable[0, 1] = display[0, 1];
+        solveSymbolTable[0, 2] = display[0, 2];
+        solveSymbolTable[1, 0] = display[1, 0];
+        solveSymbolTable[1, 1] = display[1, 1];
+        solveSymbolTable[1, 2] = display[1, 2];
+
         newSymbolTable[0, 0] = display[0, 0];
         newSymbolTable[0, 1] = display[0, 1];
         newSymbolTable[0, 2] = display[0, 2];
         newSymbolTable[1, 0] = display[1, 0];
         newSymbolTable[1, 1] = display[1, 1];
         newSymbolTable[1, 2] = display[1, 2];
+
+        
 
         var initialOrder = Enumerable.Range(0, 6).ToArray();
         ShuffleArray(initialOrder);
@@ -111,47 +127,49 @@ public class MyMomScript : MonoBehaviour
         RedrawSymbols();
     }
 
-    void RotateVertical(KMSelectable button, int column)
+    private bool RotateVertical(char[,] disp, KMSelectable button, int column)
     {
         Audio.PlaySoundAtTransform("tick", button.transform);
         button.AddInteractionPunch(0.25f);
-        if (!isSolved)
-        {
-            var temp = display[0, column - 1];
-            display[0, column - 1] = display[1, column - 1];
-            display[1, column - 1] = temp;
-            RedrawSymbols();
-        }
-        
+        RotateVertical(disp, column);
+        RedrawSymbols();
+        return false;
     }
 
-    void RotateHorizontal(KMSelectable button, int line, int direction = 0)
+    private static void RotateVertical(char[,] disp, int column)
+    {
+        var temp = disp[0, column - 1];
+        disp[0, column - 1] = disp[1, column - 1];
+        disp[1, column - 1] = temp;
+    }
+
+    private bool RotateHorizontal(char[,] disp, KMSelectable button, int line, int direction = 0)
     {
         Audio.PlaySoundAtTransform("tick", button.transform);
         button.AddInteractionPunch(0.25f);
-
-        if (!isSolved)
-        {
-            if (direction == -1)
-            {
-                var temp = display[line, 0];
-                display[line, 0] = display[line, 1];
-                display[line, 1] = display[line, 2];
-                display[line, 2] = temp;
-            }
-            else
-            {
-                var temp = display[line, 2];
-                display[line, 2] = display[line, 1];
-                display[line, 1] = display[line, 0];
-                display[line, 0] = temp;
-            }
-
-            RedrawSymbols();
-        }
-
-        
+        RotateHorizontal(disp, line, direction);
+        RedrawSymbols();
+        return false;
     }
+
+    private static void RotateHorizontal(char[,] disp, int line, int direction)
+    {
+        if (direction == -1)
+        {
+            var temp = disp[line, 0];
+            disp[line, 0] = disp[line, 1];
+            disp[line, 1] = disp[line, 2];
+            disp[line, 2] = temp;
+        }
+        else
+        {
+            var temp = disp[line, 2];
+            disp[line, 2] = disp[line, 1];
+            disp[line, 1] = disp[line, 0];
+            disp[line, 0] = temp;
+        }
+    }
+
 
     void RedrawSymbols()
     {
@@ -169,27 +187,44 @@ public class MyMomScript : MonoBehaviour
         Audio.PlaySoundAtTransform("tick", this.transform);
         GetComponent<KMSelectable>().AddInteractionPunch();
         Debug.LogFormat("[My Mom #{0}] You submitted: {1} / {2}", moduleId, new string(Enumerable.Range(0, 3).Select(i => display[0, i]).ToArray()), new string(Enumerable.Range(0, 3).Select(i => display[1, i]).ToArray()));
-
+        bool nani = false;
+        bool played = false;
+        int cheese = 0;
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 2; j++)
             {
+                
                 if (display[j, i] != newSymbolTable[y + j, x + i])
                 {
                     Debug.LogFormat("[My Mom #{0}] Wrong solution. Strike.", moduleId);
-                    BombModule.HandleStrike();
+                    nani = true;
                     Audio.PlaySoundAtTransform("screech", submitButton.transform);
+                }
+                if (display[j, i] == newSymbolTable[y + j, x + i])
+                {
+                    cheese++;
+                }
+                if (!nani && cheese == 6)
+                {
+                   isSolved = true;
                 }
             }
         }
-
-        if (!isSolved)
+        if (nani)
+        {
+            Debug.LogFormat("[My Mom #{0}] Wrong solution. Strike.", moduleId);
+            BombModule.HandleStrike();
+            nani = false;
+            Audio.PlaySoundAtTransform("screech", submitButton.transform);
+        }
+        if (isSolved && !played)
         {
             Debug.LogFormat("[My Mom #{0}] Module solved.", moduleId);
             BombModule.HandlePass();
             Audio.PlaySoundAtTransform("mother", submitButton.transform);
             StartCoroutine(Animate());
-            isSolved = true;
+            played = true;
         }
     }
 
@@ -327,4 +362,79 @@ public class MyMomScript : MonoBehaviour
         else
             return null;
     }
+
+    struct SolverQueueItem
+    {
+        public char[,] PrevConfiguration;
+        public char[,] NextConfiguration;
+        public int Button;
+    }
+
+    IEnumerator TwitchHandleForcedSolve()
+    {
+        if (isSolved)
+            yield break;
+
+        var q = new Queue<SolverQueueItem>();
+        q.Enqueue(new SolverQueueItem { Button = -1, NextConfiguration = display });
+        var already = new Dictionary<string, SolverQueueItem>();
+        var solutionKey = Enumerable.Range(0, 6).Select(ix => solveSymbolTable[y + ix / 3, x + ix % 3]).Join("");
+
+        while (q.Count > 0)
+        {
+            var item = q.Dequeue();
+            var str = Enumerable.Range(0, 6).Select(ix => item.NextConfiguration[ix / 3, ix % 3]).Join("");
+
+            if (already.ContainsKey(str))
+                continue;
+            already[str] = item;
+
+            if (str == solutionKey)
+                break;
+
+            var config = (char[,])item.NextConfiguration.Clone();
+            RotateVertical(config, 1);
+            q.Enqueue(new SolverQueueItem { PrevConfiguration = item.NextConfiguration, NextConfiguration = config, Button = 0 });
+            config = (char[,])item.NextConfiguration.Clone();
+            RotateVertical(config, 2);
+            q.Enqueue(new SolverQueueItem { PrevConfiguration = item.NextConfiguration, NextConfiguration = config, Button = 2 });
+            config = (char[,])item.NextConfiguration.Clone();
+            RotateVertical(config, 3);
+            q.Enqueue(new SolverQueueItem { PrevConfiguration = item.NextConfiguration, NextConfiguration = config, Button = 4 });
+            config = (char[,])item.NextConfiguration.Clone();
+            RotateHorizontal(config, 0, -1);
+            q.Enqueue(new SolverQueueItem { PrevConfiguration = item.NextConfiguration, NextConfiguration = config, Button = 6 });
+            config = (char[,])item.NextConfiguration.Clone();
+            RotateHorizontal(config, 0, 1);
+            q.Enqueue(new SolverQueueItem { PrevConfiguration = item.NextConfiguration, NextConfiguration = config, Button = 7 });
+            config = (char[,])item.NextConfiguration.Clone();
+            RotateHorizontal(config, 1, -1);
+            q.Enqueue(new SolverQueueItem { PrevConfiguration = item.NextConfiguration, NextConfiguration = config, Button = 8 });
+            config = (char[,])item.NextConfiguration.Clone();
+            RotateHorizontal(config, 1, 1);
+            q.Enqueue(new SolverQueueItem { PrevConfiguration = item.NextConfiguration, NextConfiguration = config, Button = 9 });
+        }
+
+        if (!already.ContainsKey(solutionKey))
+            throw new InvalidOperationException();
+
+        var buttonPresses = new List<int>();
+        var cnf = solutionKey;
+        while (cnf != null)
+        {
+            var item = already[cnf];
+            buttonPresses.Add(item.Button);
+            cnf = item.PrevConfiguration == null ? null : Enumerable.Range(0, 6).Select(ix => item.PrevConfiguration[ix / 3, ix % 3]).Join("");
+        }
+
+        for (int i = buttonPresses.Count - 2; i >= 0; i--)
+        {
+            buttons[buttonPresses[i]].OnInteract();
+            yield return new WaitForSeconds(.5f);
+        }
+
+        submitButton.OnInteract();
+        yield return new WaitForSeconds(.1f);
+    }
 }
+
